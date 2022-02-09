e430c01715464f7681ed8b9f8a7cd35c6067a3ed
TanveerCedcoss <tanveerhusain@cedcoss.com>
2022-02-09 16:03:17 +0530

    test completed

---
+++ src/script.js
@@ -0,0 +1,42 @@
+var fname, mobile;
+function fetchData(){
+    document.getElementById("na").innerHTML=" ";
+    document.getElementById("name").style="border:1px solid black" ;
+    document.getElementById("nb").innerHTML=" ";
+    document.getElementById("mobile").style="border:1px solid black" ;
+
+    fname = document.getElementById("name").value;
+    mobile= document.getElementById("mobile").value;
+    checkFields();
+
+}
+
+
+
+function checkFields()
+{
+    if(fname=='')
+    {
+        document.getElementById("na").innerHTML="This is empty!!!";
+        document.getElementById("name").style="border:1px solid red" ;
+    }
+     if(Number.isInteger(parseInt(fname)))
+     {
+        document.getElementById("na").innerHTML="Incorrect Entry!!!";
+        document.getElementById("name").style="border:1px solid red" ;
+     }
+        
+    
+    if(mobile=='')
+    {
+        document.getElementById("nb").innerHTML="This is empty!!!";
+        document.getElementById("mobile").style="border:1px solid red" ;
+    }
+    if(isNaN(mobile))
+    {
+        document.getElementById("nb").innerHTML="Incorrect entry!!!";
+        document.getElementById("mobile").style="border:1px solid red" ;
+
+    }
+}
+
